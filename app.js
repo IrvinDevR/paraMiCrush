70046884b19d2674302d020ca5a2106fdd8c16d2
Rubén Mantilla <lx05551@innovaccion.mx>
2022-11-15 03:16:07 -0500

    change design

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ let divModoSexo = document.getElementsByClassName("modo_sexo")[0];
 btnNo.addEventListener('mouseenter', function(e) { moverPositionRandom(e.target) });
 
 btnSi.addEventListener('click', function(e) {
-    alert('Sabía que dirías que SÍ. Casémonos ya y tengamos hijos. TE AMO!!! 😍');
+    alert('Sabía que dirías que SÍ. Casémonos ya. TE AMO!!! 😍');
 
-    divModoSexo.style.display = "block";
-    const cancion = new Audio('mp3\\Rulo_Contrabanda_Por_Ti.mp3');
+    divModoSexo.style.display = "flex";
+    const cancion = new Audio('music\\Rulo_Contrabanda_Por_Ti.mp3');
     cancion.play();
 })
